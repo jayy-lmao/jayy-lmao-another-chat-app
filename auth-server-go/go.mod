4f59342eeedd11978c469887ce56a2235e8127b0
James Holman <james.holman@domain.com.au>
2020-04-10 15:40:24 +1000

    :alembic: moving to uuids

--- auth-server-go/go.mod
+++ auth-server-go/go.mod
@@ -6,6 +6,9 @@ require (
 	github.com/cweill/gotests v1.5.3 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/jinzhu/gorm v1.9.12
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/satori/go.uuid v1.2.0 // indirect
 	golang.org/x/crypto v0.0.0-20191205180655-e7c4368fe9dd
 	golang.org/x/tools v0.0.0-20200409210453-700752c24408 // indirect
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 )
--- auth-server-go/types.go
+++ auth-server-go/types.go
@@ -1,29 +1,39 @@
 package main 
 
+
+
 import (
-  "github.com/jinzhu/gorm")
+  "github.com/jinzhu/gorm"
+  "github.com/satori/go.uuid"
+)
 
-  type HasuraClaims struct {
-    Roles []string `json:"X-Hasura-Allowed-Roles"`
-    UserId string `json:"X-Hasura-User-Id"`
-    DefaultRole string `json:"X-Hasura-Default-Role"`
-    Role string `json:"X-Hasura-Role"`
-  }
+type HasuraClaims struct {
+  Roles []string `json:"X-Hasura-Allowed-Roles"`
+  UserId string `json:"X-Hasura-User-Id"`
+  DefaultRole string `json:"X-Hasura-Default-Role"`
+  Role string `json:"X-Hasura-Role"`
+}
 
-  type LoginDetails struct {
-    Username string `json:"username"`
-    Password string `json:"password"`
-  }
+type LoginDetails struct {
+  Username string `json:"username"`
+  Password string `json:"password"`
+}
 
-  type SignupDetails struct {
-    Username string `json:"username"`
-    Password string `json:"password"`
-    Displayname string `json:"displayname"`
-  }
+type SignupDetails struct {
+  Username string `json:"username"`
+  Password string `json:"password"`
+  Displayname string `json:"displayname"`
+}
 
-  type User struct {
-    gorm.Model
-    Username string `gorm:"username"`
-    Password string `gorm:"password"`
-    Displayname string `gorm:"displayname"`
-  }
+type User struct {
+  gorm.Model
+  ID  uuid.UUID `gorm:"column:id;type:uuid;primary_key;"`
+  Username string `gorm:"username"`
+  Password string `gorm:"password"`
+  Displayname string `gorm:"displayname"`
+}
+
+func (base *User) BeforeCreate(scope *gorm.Scope) error {
+  uuid := uuid.NewV4()
+  return scope.SetColumn("ID", uuid)
+}
