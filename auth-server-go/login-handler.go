8685aa89b11416ef9469b45017ad48850d1629ea
James Holman <james.holman@domain.com.au>
2020-04-10 13:55:25 +1000

    :alembic: changing the way bcrypt compares

--- auth-server-go/login-handler.go
+++ auth-server-go/login-handler.go
@@ -27,43 +27,42 @@ func LoginHandler(w http.ResponseWriter, r *http.Request){
     fmt.Fprintf(w, "Please enter a username and password")
   }
   json.Unmarshal(reqBody, &loginDetails)
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(loginDetails.Password), 8)
 
   var user User;
 
   db.First(&user, "username = ?", loginDetails.Username)
 
   log.Printf("%+v", user)
-  log.Print(string(hashedPassword))
 
   // Where we should do the database lookup
-  if string(hashedPassword) != user.Password {
-    w.WriteHeader(http.StatusUnauthorized)
-    io.WriteString(w, `{"error" : "invalid_credentials"}`)
-    return
-  }
+  bcryptErr := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(loginDetails.Password))
 
-  hasuraClaims := &HasuraClaims{
-    Role: "user",
-    UserId: user.Model.ID,
-    DefaultRole: "user",
-    Roles: `["mine","user"]`,
-  }
-
-  token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-    "user": loginDetails.Username,
-    "exp": time.Now().Add(time.Hour * time.Duration(1)).Unix(),
-    "iat": time.Now().Unix(),
-    "https://hasura.io/jwt/claims": hasuraClaims,
-  })
-
-  tokenString, err := token.SignedString([]byte(JWT_SECRET))
-
-  if err != nil {
-    w.WriteHeader(http.StatusInternalServerError)
-    io.WriteString(w, `{"error":"token_gen_failed"}`)
-    return
-  }
-  io.WriteString(w,`{"token":"`+tokenString+`"}`)
+  if bcryptErr != nil { w.WriteHeader(http.StatusUnauthorized)
+  io.WriteString(w, `{"error" : "invalid_credentials"}`)
   return
+}
+
+hasuraClaims := &HasuraClaims{
+  Role: "user",
+  UserId: user.Model.ID,
+  DefaultRole: "user",
+  Roles: `["mine","user"]`,
+}
+
+token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+  "user": loginDetails.Username,
+  "exp": time.Now().Add(time.Hour * time.Duration(1)).Unix(),
+  "iat": time.Now().Unix(),
+  "https://hasura.io/jwt/claims": hasuraClaims,
+})
+
+tokenString, err := token.SignedString([]byte(JWT_SECRET))
+
+if err != nil {
+  w.WriteHeader(http.StatusInternalServerError)
+  io.WriteString(w, `{"error":"token_gen_failed"}`)
+  return
+}
+io.WriteString(w,`{"token":"`+tokenString+`"}`)
+return
 }
