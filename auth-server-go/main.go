b61c83ec991601829ab95bdaf4745d8df2e11db0
James Holman <james.holman@domain.com.au>
2020-04-10 11:44:31 +1000

    :alembic: using port now

--- auth-server-go/main.go
+++ auth-server-go/main.go
@@ -3,12 +3,16 @@ package main
 import (
   "log"
   "net/http"
+  "os"
 )
 
 func main(){
-  http.HandleFunc("/login", LoginHandler)
-  if err:= http.ListenAndServe(":8080",nil);err!= nil {
-    log.Fatal(err)
+  port := os.Getenv("PORT")
+  if port == "" {
+    log.Fatal("$PORT must be set")}
+    http.HandleFunc("/login", LoginHandler)
+    if err:= http.ListenAndServe(":"+port,nil);err!= nil {
+      log.Fatal(err)
+    }
   }
-}
 
