0961e496975070986c5abdfa47c65e83415d57f8
jayy-lmao <jameswarringtonholman@gmail.com>
2020-05-14 18:02:19 +1000

    :sparkles: you can now send messages if you are logged in

--- web/src/components/MessageBox.tsx
+++ web/src/components/MessageBox.tsx
@@ -1,6 +1,24 @@
 import { h, JSX } from "preact";
 import { useState } from "preact/compat";
+import { useMutation } from "graphql-hooks";
 import "../scss/message.scss";
+
+const SEND_MESSAGE_MUTATION = `
+mutation sendMessage($chatId:Int!,$content: String!) {
+  insert_message(objects:{
+    chat_id: $chatId
+    content: $content
+  }){
+    returning{
+      id
+      content
+      sender {
+        displayname
+      }
+    }
+  }
+}
+`
 
 interface MessageBoxProps {
   chatId: string;
@@ -11,12 +29,16 @@ type ClickEvent = JSX.TargetedEvent<HTMLButtonElement, Event>;
 
 function MessageBox({ chatId }: MessageBoxProps) {
   const [message, setMessage] = useState("");
+  const [sendMessage] = useMutation(SEND_MESSAGE_MUTATION)
   const handleChange = ({ currentTarget }: InputEvent) => {
     setMessage(currentTarget.value);
   };
   const handleClick = (e: ClickEvent) => {
     e.preventDefault();
-    console.log({ message, chatId });
+    sendMessage({variables: {
+      chatId: parseInt(chatId),
+      content: message
+    }})
     setMessage("");
   };
   return (
